719eb88f817c15bef972a1109895dbeb82165889
Jinniepoo <118496261+Jinniepoo@users.noreply.github.com>
2025-08-18 20:46:08 +0900

    Update RandomStateMachineBehaviour.cs

--- Assets/Scripts/Characters/AnimatorStateMachineBehaviours/RandomStateMachineBehaviour.cs
+++ Assets/Scripts/Characters/AnimatorStateMachineBehaviours/RandomStateMachineBehaviour.cs
@@ -16,19 +16,18 @@ namespace Diablo.Characters
 
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            // Randomly decide a time at which to transition.
             randomNormalTime = Random.Range(minNormTime, maxNormTime);
         }
 
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            // If transitioning awy from this state reset the random idle parameter to -1.
+            // 이 상태에서 전환될 경우, 랜덤 Idle 파라미터를 -1로 초기화
             if (animator.IsInTransition(0) && animator.GetCurrentAnimatorStateInfo(0).fullPathHash == stateInfo.fullPathHash)
             {
                 animator.SetInteger(hashRandomIdle, -1);
             }
 
-            // If the state is beyond the randomly decided normalised time and not yet transitioning then set a random idle.
+            // 상태가 무작위로 결정된 Normalised Time)을 넘겼고 아직 전환 중이 아니라면 랜덤 Idle로 설정
             if (stateInfo.normalizedTime > randomNormalTime && !animator.IsInTransition(0))
             {
                 animator.SetInteger(hashRandomIdle, Random.Range(0, numberOfStates));
